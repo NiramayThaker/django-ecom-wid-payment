c689c9d24166ed1b71433d038be378d52fda3ace
Admin <thakerniramay@gmail.com>
2024-05-29 16:19:56 +0530

    Setting up templates

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('contact-us', views.contact_us, name='contact-us'),
+    path('about-us', views.about_us, name='about-us'),
+    path('sign-in', views.sign_in, name='sign-in'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
+
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+
+def sign_in(request):
+    return render(request, 'sign-in.html')
